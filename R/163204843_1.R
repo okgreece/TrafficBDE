6e27d84daa018f0025e7e3ef94e20f55e5788013
katerinachat <katerinachat@yahoo.com>
2017-07-27 23:21:35 +0300

    DESCRIPTION csv

---
+++ R/163204843_1.R
@@ -0,0 +1,25 @@
+#' Sample data from Traffic BDE
+#' 
+#' @description 
+#' Sample data of the traffic data of the road with Lik id "163204843" and direction = "1"
+#' \itemize{
+#'   \item The record number of the initial data
+#'   \item The Link id of the road
+#'   \item The direction of the road
+#'   \item The date and time of the recorded arguments
+#'   \item The min speed each time
+#'   \item The max speed each time
+#'   \item The mean speed each time
+#'   \item The standard deviation of the speed
+#'   \item The skewness of the speed
+#'   \item The kurtosis of the speed
+#'   \item The entries each time
+#'   \item The unique entries each time
+#'   }
+#' 
+#' @format A csv file
+#' @name 163204843_1
+#' @source TrafficBDE
+#' 
+#' 
+NULL
